d8d0a5cd4ad9358e5253468412d2b4f637859a77
Isaac Cheng <isaac.cheng97@gmail.com>
2023-01-11 19:27:28 -0700

    curl Part 10

--- lab1.py
+++ lab1.py
@@ -4,4 +4,6 @@ print(requests.__version__)
 
 x = requests.get('http://google.com')
 
-print(x.status_code)
+r = requests.get('https://raw.githubusercontent.com/Isaac415/CMPUT404Lab1/main/lab1.py')
+
+print(r.text)
